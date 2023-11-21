6dd96a5d6866e906390007126a03d5cd318a0e70
PietroCipolla95 <pietro.cipolla.95@gmail.com>
2023-11-21 03:19:03 +0100

    fix visual bug in show

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -22,58 +22,61 @@
                     alt="{{ $project->title . 'image' }}">
             </div>
             {{-- right column with description and links --}}
-            <div class="col">
-                <div>
-                    <div>
-                        <h5>
-                            Technologies Used:
-                        </h5>
-                        @forelse ($project->technologies as $technology)
-                            <span class="text-info fw-semibold">
-                                {{ $technology->name }}
-                            </span>
-                            <span>
-                                /
-                            </span>
-                        @empty
-                            <span class="fw-semibold text-info">
-                                No technology listed
-                            </span>
-                        @endforelse
+            <div class="col-6">
+                <div class="card bg-secondary border-dark p-2">
 
-                    </div>
-                    <h5 class="py-3">
-                        Type:
-                        <span class="text-info">
-                            {{ $project->type_id == null ? 'no type selected' : $project->type?->name }}
-                        </span>
-                    </h5>
-                    <h4>
-                        Description
-                    </h4>
-                    <p>
-                        {{ $project->description }}
-                    </p>
-                </div>
-                {{-- projects links wrapper --}}
-                <div>
-                    {{-- git hub --}}
-                    <div class="pt-5 pb-4">
-                        <h5>
-                            Git Hub <i class="fa-brands fa-github"></i>
-                        </h5>
-                        <a href="{{ $project->git_link }}" class="text-decoration-none text-info">
-                            {{ $project->git_link }}
-                        </a>
-                    </div>
-                    {{-- project link --}}
                     <div>
-                        <h5>
-                            Project <i class="fa-solid fa-earth-europe"></i>
+                        <div>
+                            <h5>
+                                Technologies Used:
+                            </h5>
+                            @forelse ($project->technologies as $technology)
+                                <span class="text-info fw-semibold">
+                                    {{ $technology->name }}
+                                </span>
+                                <span>
+                                    /
+                                </span>
+                            @empty
+                                <span class="fw-semibold text-info">
+                                    No technology listed
+                                </span>
+                            @endforelse
+
+                        </div>
+                        <h5 class="py-3">
+                            Type:
+                            <span class="text-info">
+                                {{ $project->type_id == null ? 'no type selected' : $project->type?->name }}
+                            </span>
                         </h5>
-                        <a href="{{ $project->project_link }}" class="text-decoration-none text-info">
-                            {{ $project->project_link }}
-                        </a>
+                        <h4>
+                            Description
+                        </h4>
+                        <p>
+                            {{ $project->description }}
+                        </p>
+                    </div>
+                    {{-- projects links wrapper --}}
+                    <div>
+                        {{-- git hub --}}
+                        <div class="pt-5 pb-4">
+                            <h5>
+                                Git Hub <i class="fa-brands fa-github"></i>
+                            </h5>
+                            <a href="{{ $project->git_link }}" class="text-decoration-none text-info">
+                                {{ $project->git_link }}
+                            </a>
+                        </div>
+                        {{-- project link --}}
+                        <div>
+                            <h5>
+                                Project <i class="fa-solid fa-earth-europe"></i>
+                            </h5>
+                            <a href="{{ $project->project_link }}" class="text-decoration-none text-info">
+                                {{ $project->project_link }}
+                            </a>
+                        </div>
                     </div>
                 </div>
 
